d8e60f0227875b759027a9fa72f79e5db81646f2
alejandrotovar9 <jatovar02@gmail.com>
2024-01-14 22:03:23 -0400

    commit inicial, working

---
+++ Prueba_NodeRED_Mosquitto/include/wifi_credentials.h
@@ -0,0 +1,8 @@
+#ifndef WIFI_CREDENTIALS_H
+#define WIFI_CREDENTIALS_H
+
+//Parametros para conexion WiFi
+const char* ssid     = "JET Home";
+const char* password = "chemundo300665";
+
+#endif // WIFI_CREDENTIALS_H
