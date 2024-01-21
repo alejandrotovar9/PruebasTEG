80844697e902c39a7b7ce56915ca759f34d00b73
alejandrotovar9 <jatovar02@gmail.com>
2024-01-21 09:24:07 -0400

    envio de byte arrays con exito

--- PruebasLoRa/src/main.cpp
+++ PruebasLoRa/src/main.cpp
@@ -1,17 +1,34 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <LoRa.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/queue.h>
 
 int counter = 0;
 
-#define ss 4
-#define rst 5
+#define ss 5
+#define rst 14
 #define dio0 2
 
-void setup() {
-   Serial.begin(115200); 
-  while (!Serial);
-  Serial.println("LoRa Sender");
+// //Handle de la Cola
+// QueueHandle_t xQueue;
+// QueueHandle_t dataQueue;
+
+//float prueba[10];
+
+float floatArray[] = {3.14, 1.23, 4.56, 2.34, 5.67};
+byte* byteArray = reinterpret_cast<byte*>(floatArray);
+size_t byteArraySize = sizeof(floatArray);
+
+// //Handle de tareas
+// TaskHandle_t xHandle_mqttTask;
+TaskHandle_t xHandle_lora_send_packet;
+
+void lora_setup(void){
+    //La libreria crea un objeto LoRa de la clase LoRaClass
+
+  LoRa.setSpreadingFactor(6); //valor entre 6 y 12
  
   LoRa.setPins(ss, rst, dio0);    //setup LoRa transceiver module
   
@@ -24,16 +41,40 @@ void setup() {
   Serial.println("LoRa Initializing OK!");
 }
 
+
+void lora_send_packet(void *Parameters){
+  while(1)
+    {  
+      Serial.print("Sending packet: ");
+      Serial.println(counter);
+    
+      byte data[] = {0x01, 0x02, 0x03, 0x04};
+
+      LoRa.beginPacket();   //Send LoRa packet to receiver
+      //LoRa.print(" ");
+      LoRa.write(data, sizeof(data));
+      //LoRa.print(counter);
+      LoRa.endPacket();
+
+      counter++;
+    
+      vTaskDelay(2000/portTICK_PERIOD_MS);
+    }
+}
+
 void loop() {
- Serial.print("Sending packet: ");
-  Serial.println(counter);
- 
-  LoRa.beginPacket();   //Send LoRa packet to receiver
-  LoRa.print("hello ");
-  LoRa.print(counter);
-  LoRa.endPacket();
- 
-  counter++;
- 
-  delay(5000);
+ //Do nothing...
+}
+
+void setup() {
+   Serial.begin(115200); 
+  while (!Serial);
+
+  Serial.println("LoRa Sender");
+
+  lora_setup();
+
+  //Tareas a ejecutarse en el Nucleo 1 (lectura de hum y temp)
+  // xTaskCreatePinnedToCore(readBMETask, "readBMETask", 1024*2, NULL, 1, &xHandle_readBMETask, 1);
+  xTaskCreatePinnedToCore(lora_send_packet, "lora_send_packet", 1024*2, NULL, 1, &xHandle_lora_send_packet, 1);
 }
