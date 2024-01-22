8e61e90bacdda0c25bd52d186606864f7ff67728
alejandrotovar9 <jatovar02@gmail.com>
2024-01-22 15:12:04 -0400

    prueba duplex funcional

--- PruebasLora_Receptor/src/main.cpp
+++ PruebasLora_Receptor/src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <LoRa.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/queue.h>
 
 
 #define ss 5
@@ -8,9 +11,48 @@
 #define dio0 2
 #define LED 4
 
-void lora_setup(void){
-  LoRa.setSpreadingFactor(6); //Valor entre 6 y 12
+const int maxPayloadSize = 10;
 
+// //Handle de tareas
+TaskHandle_t xHandle_lora_receive_packet;
+TaskHandle_t xHandle_lora_send_packet;
+
+void lora_receive_packet(void *pvParameters){
+   // Check if there is any incoming data
+  while(1)
+  { 
+    if (LoRa.parsePacket()) {
+      Serial.print("Received packet: ");
+      //Read data and print it
+      byte receivedData[maxPayloadSize];
+      size_t dataSize = LoRa.readBytes(receivedData, maxPayloadSize);
+
+      for (size_t i = 0; i < dataSize; i++) {
+        Serial.print(receivedData[i]);
+        Serial.print(" ");
+      }
+      Serial.println();
+
+      //Se envia mensaje de confirmacion
+      //vTaskResume(&xHandle_lora_send_packet);
+    }
+  }  
+  vTaskDelay(100/portTICK_PERIOD_MS); // Adjust the delay based on your requirements
+}
+
+void lora_send_packet(void *Parameters){
+  while(1)
+    {  
+      Serial.print("Sending confirmation...");
+
+      LoRa.beginPacket();   //Send LoRa packet to receiver
+      LoRa.print("Si");
+      LoRa.endPacket();
+      vTaskSuspend(&xHandle_lora_send_packet);
+    }
+}
+
+void lora_setup(void){
   LoRa.setPins(ss, rst, dio0);    //setup LoRa transceiver module
  
   while (!LoRa.begin(433E6))     //433E6 - Asia, 866E6 - Europe, 915E6 - North America
@@ -18,6 +60,7 @@ void lora_setup(void){
     Serial.println(".");
     delay(500);
   }
+  LoRa.setSpreadingFactor(6); //Valor entre 6 y 12
   LoRa.setSyncWord(0xA5);
   Serial.println("LoRa Initializing OK!");
 }
@@ -27,24 +70,13 @@ void setup() {
   while (!Serial);
   Serial.println("LoRa Receiver");
   lora_setup();
+
+  xTaskCreatePinnedToCore(lora_receive_packet, "lora_receive_packet", 1024*2, NULL, 1, &xHandle_lora_receive_packet, 1);  
+  // xTaskCreatePinnedToCore(lora_send_packet, "lora_send_packet", 1024*2, NULL, 1, &xHandle_lora_send_packet, 0);
+  // vTaskSuspend(&xHandle_lora_send_packet);  
+
 }
 
 void loop() {
-  int packetSize = LoRa.parsePacket();    // try to parse packet
-  if (packetSize) 
-  {
-    
-    digitalWrite(LED, HIGH);
-    Serial.print("Received packet '");
- 
-    while (LoRa.available())              // read packet
-    {
-      String LoRaData = LoRa.readString();
-      Serial.print(LoRaData); 
-    }
-    Serial.print("' with RSSI ");         // print RSSI of packet
-    Serial.println(LoRa.packetRssi());
-    //delay(2000);
-    digitalWrite(LED,LOW);
-  }
+    //do nothing
   }
