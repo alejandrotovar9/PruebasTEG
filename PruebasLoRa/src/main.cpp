c5d934005a4512ef9a2e427eb83aa025a3439b97
alejandrotovar9 <jatovar02@gmail.com>
2024-02-05 18:20:13 -0400

    working LoRa Tx/RX before changes

--- PruebasLoRa/src/main.cpp
+++ PruebasLoRa/src/main.cpp
@@ -5,6 +5,7 @@
 #include <freertos/task.h>
 #include <freertos/queue.h>
 
+const int maxPayloadSize = 255;
 int counter = 0;
 
 #define ss 5
@@ -24,12 +25,12 @@ size_t byteArraySize = sizeof(floatArray);
 // //Handle de tareas
 // TaskHandle_t xHandle_mqttTask;
 TaskHandle_t xHandle_lora_send_packet;
+TaskHandle_t xHandle_lora_receptor;
+
 
 void lora_setup(void){
-    //La libreria crea un objeto LoRa de la clase LoRaClass
-
+  //La libreria crea un objeto LoRa de la clase LoRaClass
   LoRa.setSpreadingFactor(6); //valor entre 6 y 12
- 
   LoRa.setPins(ss, rst, dio0);    //setup LoRa transceiver module
   
   while (!LoRa.begin(433E6))     //433E6 - Asia, 866E6 - Europe, 915E6 - North America
@@ -41,6 +42,51 @@ void lora_setup(void){
   Serial.println("LoRa Initializing OK!");
 }
 
+void lora_receptor(void *pvParameters){
+   // Check if there is any incoming data
+  while(1)
+  { 
+    String LoRaData;
+    // LoRa data packet size received from LoRa sender
+    int packetSize = LoRa.parsePacket();
+    // if the packer size is not 0, then execute this if condition
+    //Serial.println("Esperando datos...");
+    if (packetSize) {
+      // received a packet
+      Serial.print("Received packet: ");
+
+      // receiving the data from LoRa sender
+      while (LoRa.available()) {
+        LoRaData = LoRa.readString();
+      }
+      Serial.println(LoRaData);
+    }
+    vTaskDelay(200/portTICK_PERIOD_MS);
+  }
+
+  //  // Check if there is any incoming data
+  // while(1)
+  // {
+  //   Serial.println("Esperando datos...");
+  //   if (LoRa.parsePacket()) {
+  //     Serial.print("Received packet from ESP32 2 (Receptor/E.B): ");
+  //     //Read data and print it
+  //     char receivedData[maxPayloadSize];
+  //     size_t dataSize = LoRa.readBytes(receivedData, maxPayloadSize);
+
+  //     Serial.println(receivedData);
+
+  //     //Se envia siguiente mensaje
+  //     // vTaskResume(&xHandle_lora_send_packet);
+  //     // vTaskSuspend(&xHandle_lora_receptor);
+  //   }
+  //   vTaskDelay(500/portTICK_PERIOD_MS);
+
+  //   // else{
+  //   //   //Do nothing
+  //   // }
+  // }
+}
 
 void lora_send_packet(void *Parameters){
   while(1)
@@ -48,17 +94,22 @@ void lora_send_packet(void *Parameters){
       Serial.print("Sending packet: ");
       Serial.println(counter);
     
-      byte data[] = {0x01, 0x02, 0x03, 0x04};
+      //byte data[10] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10};
+      //data[0] = counter; 
 
       LoRa.beginPacket();   //Send LoRa packet to receiver
-      //LoRa.print(" ");
-      LoRa.write(data, sizeof(data));
+      LoRa.print("Hola ");
+      //LoRa.write(data, sizeof(data));
       //LoRa.print(counter);
       LoRa.endPacket();
 
       counter++;
-    
-      vTaskDelay(2000/portTICK_PERIOD_MS);
+
+      // vTaskResume(&xHandle_lora_receptor);
+      // vTaskSuspend(&xHandle_lora_send_packet);
+      int num = random(100) + 2000;
+
+      vTaskDelay(num/portTICK_PERIOD_MS);
     }
 }
 
@@ -76,5 +127,7 @@ void setup() {
 
   //Tareas a ejecutarse en el Nucleo 1 (lectura de hum y temp)
   // xTaskCreatePinnedToCore(readBMETask, "readBMETask", 1024*2, NULL, 1, &xHandle_readBMETask, 1);
-  xTaskCreatePinnedToCore(lora_send_packet, "lora_send_packet", 1024*2, NULL, 1, &xHandle_lora_send_packet, 1);
+  xTaskCreatePinnedToCore(lora_send_packet, "lora_send_packet", 1024*1, NULL, 1, &xHandle_lora_send_packet, 1);
+  //xTaskCreatePinnedToCore(lora_receptor, "lora_receptor", 1024*1, NULL, 1, &xHandle_lora_receptor, 0);
+  // vTaskSuspend(&xHandle_lora_receptor);
 }
--- Pruebas_LoRa_ByteArraySendReceive/src/main.cpp
+++ Pruebas_LoRa_ByteArraySendReceive/src/main.cpp
@@ -23,6 +23,15 @@ const int csPin = 5;          // LoRa radio chip select
 const int resetPin = 14;       // LoRa radio reset
 const int irqPin = 2;         // change for your board; must be a hardware interrupt pin
 
+// //Handle de tareas
+TaskHandle_t xHandle_poll_packet;
+TaskHandle_t xHandle_send_packet;
+TaskHandle_t xHandle_timer;
+
+float floatArray[] = {3.14, 1.23, 4.56, 2.34, 5.67};
+byte* byteArray = reinterpret_cast<byte*>(floatArray);
+size_t byteArraySize = sizeof(floatArray);
+
 String outgoing;              // outgoing message
 
 byte msgCount = 0;            // count of outgoing messages
@@ -33,15 +42,97 @@ long lastSendTime = 0;        // last send time
 int interval = 2000;          // interval between sends
 
 
-void sendMessage(String outgoing) {
+void leer_datos(int packetSize){
+
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  Serial.println("Hay mensaje");
+
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+
+  byte incoming[incomingLength]; //Crea un byte array del tamaño de los datos enviados
+  	
+  //Guardo datos en un vector para su posterior utilización
+  //Existe manera mas rápida de guardarlos?
+  int k = 0;
+  while (LoRa.available()) {
+    incoming[k] = (byte)LoRa.read();
+    k++;
+  }
+
+  // if (incomingLength != incoming.length()) {   // check length for error
+  //   Serial.println("error: message length does not match length");
+  //   return;                             // skip rest of function
+  // }
+
+  //Revisar por que se reinicia l micro en esta excepcion
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != 0xFF) {
+    Serial.println("This message is not for me.");
+    //Intento de evitar reinicio del micro...
+    vTaskDelay(100/portTICK_PERIOD_MS);
+    return;                             // skip rest of function
+  }
+
+   // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  //Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
+  printf("El primer byte del mensaje es: %x",incoming[4]); //Imprime un byte, placeholder "x"
+}
+
+void sendMessage(size_t size_data, byte data[]) {
   LoRa.beginPacket();                   // start packet
   LoRa.write(destination);              // add destination address
   LoRa.write(localAddress);             // add sender address
   LoRa.write(msgCount);                 // add message ID
-  LoRa.write(outgoing.length());        // add payload length
-  LoRa.print(outgoing);                 // add payload
+  LoRa.write(size_data);      // add payload length
+  //LoRa.print(outgoing);                 // add payload
+  LoRa.write(data, size_data);
   LoRa.endPacket();                     // finish packet and send it
   msgCount++;                           // increment message ID
+}
+
+void poll_packet(void *pvParameters){
+  while(1){
+    int packetSize;
+    packetSize = LoRa.parsePacket();
+    leer_datos(packetSize);
+    //Serial.println("No han llegado datos...");
+    vTaskDelay(10/portTICK_PERIOD_MS);
+
+    //Activa la tarea de envio de datos una vez se recibe algo
+    //vTaskResume(xHandle_send_packet);
+  }
+}
+
+void send_packet(void *pvParameters){
+  while(1){
+    //Byte array a enviar
+    size_t size_data;
+    byte data[]={0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02,0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03}; 
+
+    size_data = sizeof(data);
+
+    //Llamo a la funcion que crea la trama de datos (payload)
+    sendMessage(size_data, data);
+    Serial.println("Sending byte array");
+
+    //Espero 2 segundos luego de enviar mensaje
+    vTaskDelay(2000/portTICK_PERIOD_MS);
+
+    //Suspendo esta tarea
+    //vTaskSuspend(NULL);
+  }  
 }
 
 void onReceive(int packetSize) {
@@ -97,17 +188,25 @@ void setup() {
   }
 
   Serial.println("LoRa init succeeded.");
+
+  //Tareas de FreeRTOS corriendo en el nucleo 0 del ESP32
+
+  xTaskCreatePinnedToCore(send_packet, "send_packet", 1024*2, NULL, 1, &xHandle_send_packet, 0);
+  //vTaskSuspend(xHandle_send_packet);
+  xTaskCreatePinnedToCore(poll_packet, "poll_packet", 1024*2, NULL, 1, &xHandle_poll_packet, 0);
+
+  //xTaskCreatePinnedToCore(timer, "timer", 1024*2, NULL, 1, &xHandle_timer, 0);
 }
 
 void loop() {
-  if (millis() - lastSendTime > interval) {
-    String message = "HeLoRa World!";   // send a message
-    sendMessage(message);
-    Serial.println("Sending " + message);
-    lastSendTime = millis();            // timestamp the message
-    interval = 2000;    // 2-3 seconds
-  }
+  // if (millis() - lastSendTime > interval) {
+  //   String message = "HeLoRa World!";   // send a message
+  //   sendMessage(message);
+  //   Serial.println("Sending " + message);
+  //   lastSendTime = millis();            // timestamp the message
+  //   interval = 2000;    // 2-3 seconds 
+  // }
 
   // parse for a packet, and call onReceive with the result:
-  onReceive(LoRa.parsePacket());
+  //onReceive(LoRa.parsePacket());
 }
