68b0c132b95fcb59cb34823fd877a8f45a5b29e2
alejandrotovar9 <jatovar02@gmail.com>
2024-02-06 11:31:26 -0400

    codigo funcional del receptor, sin reboots

--- Pruebas_LoRa_ByteArraySendReceive/src/main.cpp
+++ Pruebas_LoRa_ByteArraySendReceive/src/main.cpp
@@ -36,15 +36,16 @@ String outgoing;              // outgoing message
 
 byte msgCount = 0;            // count of outgoing messages
 //OJO UNO ES 0xFF y el otro 0xbb, para que tengan diferentes identificadores
-byte localAddress = 0xFF;     // address of this device
-byte destination = 0xBB;      // destination to send to
+byte localAddress = 0xBB;     // address of this device
+byte destination = 0xFF;      // destination to send to
 long lastSendTime = 0;        // last send time
 int interval = 2000;          // interval between sends
+int expected_length = 125;
 
 
-void leer_datos(int packetSize){
+int leer_datos(int packetSize){
 
-  if (packetSize == 0) return;          // if there's no packet, return
+  if (packetSize == 0) return 0;          // if there's no packet, return
 
   Serial.println("Hay mensaje");
 
@@ -53,6 +54,11 @@ void leer_datos(int packetSize){
   byte sender = LoRa.read();            // sender address
   byte incomingMsgId = LoRa.read();     // incoming msg ID
   byte incomingLength = LoRa.read();    // incoming msg length
+
+  if (incomingLength != expected_length) {   // check length for error
+    Serial.println("error: message length does not match length");
+    return 1;                             // skip rest of function
+  }
 
   byte incoming[incomingLength]; //Crea un byte array del tamaño de los datos enviados
   	
@@ -64,18 +70,15 @@ void leer_datos(int packetSize){
     k++;
   }
 
-  // if (incomingLength != incoming.length()) {   // check length for error
-  //   Serial.println("error: message length does not match length");
-  //   return;                             // skip rest of function
-  // }
-
   //Revisar por que se reinicia l micro en esta excepcion
   // if the recipient isn't this device or broadcast,
   if (recipient != localAddress && recipient != 0xFF) {
     Serial.println("This message is not for me.");
-    //Intento de evitar reinicio del micro...
-    vTaskDelay(100/portTICK_PERIOD_MS);
-    return;                             // skip rest of function
+    //Reescribiendo en el buffer que ya tenia? Por eso el error?
+    //Vaciando arreglo...
+    //incoming[incomingLength] = {};
+    //vTaskDelay(1000/portTICK_PERIOD_MS);
+    return 2;                             // skip rest of function
   }
 
    // if message is for this device, or broadcast, print details:
@@ -86,8 +89,11 @@ void leer_datos(int packetSize){
   //Serial.println("Message: " + incoming);
   Serial.println("RSSI: " + String(LoRa.packetRssi()));
   Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println("El primer byte del mensaje es: " + String(incoming[0], HEX));
   Serial.println();
-  printf("El primer byte del mensaje es: %x",incoming[4]); //Imprime un byte, placeholder "x"
+
+  //Si llega aca devuelvo true
+  return 3;
 }
 
 void sendMessage(size_t size_data, byte data[]) {
@@ -104,74 +110,99 @@ void sendMessage(size_t size_data, byte data[]) {
 
 void poll_packet(void *pvParameters){
   while(1){
-    int packetSize;
-    packetSize = LoRa.parsePacket();
-    leer_datos(packetSize);
-    //Serial.println("No han llegado datos...");
-    vTaskDelay(10/portTICK_PERIOD_MS);
+    //Serial.println("Polling...");
+    int packetSize = LoRa.parsePacket();
+    int flag = leer_datos(packetSize);
 
-    //Activa la tarea de envio de datos una vez se recibe algo
-    //vTaskResume(xHandle_send_packet);
-  }
+    //--------------------------Manejo de excepciones de receptor------------------------------
+    switch(flag){
+      case 0: break;//No se recibio un paquete, seguir haciendo polling
+      case 1: 
+        Serial.println("Los datos estan corruptos");
+        break;//La data no es de la longitud deseada (Data corrupted)
+      case 2:
+        Serial.println("#####################################################");
+        Serial.println("Los datos estan corruptos o no son para este receptor");
+        Serial.println("#####################################################");
+        break;
+         //El mensaje no es para el
+      case 3:
+        Serial.println("Se recibio un paquete!!! Reactivando tarea de envio de mensaje...");
+        vTaskResume(xHandle_send_packet); //La data llego con exito y tiene el formato deseado
+        //Aqui se enviarian los datos para ser guardados y enviados por MQTT
+    }
+    // if(leer_datos(packetSize)){ //AQUI PUEDE IR EL CONTADOR DE IDs para enviar mensaje de verificacion
+    //   //Se recibio algo
+    //   Serial.println("Se recibio un paquete!!! Reactivando tarea de envio de mensaje...");
+    //   vTaskResume(xHandle_send_packet);
+    // }
+    // else{
+    //   //vTaskDelay(1000/portTICK_PERIOD_MS);
+    // }
+      // if(leer_datos(packetSize) == 1){
+      //   Serial.println("Recepcion correcta");
+      // }
+      // else{
+      //   Serial.println("Error en el envio...");
+      //   vTaskDelay(500/portTICK_PERIOD_MS);
+      // }
+
+      //Serial.println("No han llegado datos...");
+      vTaskDelay(10/portTICK_PERIOD_MS);
+
+      //Activa la tarea de envio de datos una vez se recibe algo
+      //vTaskResume(xHandle_send_packet);
+    }
+}
+
+void send_packet_nollego(void *pvParameters){
+  while(1){
+    //Byte array a enviar
+    size_t size_data;
+    byte data[]={0x00}; 
+    size_data = sizeof(data);
+
+    //Llamo a la funcion que crea la trama de datos (payload)
+    sendMessage(size_data, data);
+    Serial.println("Sending byte array de respuesta negativa!");
+
+    //Espero 2 segundos luego de enviar mensaje
+    //vTaskDelay(2000/portTICK_PERIOD_MS);
+
+    //vTaskResume(xHandle_poll_packet);
+
+    //Suspendo esta tarea hasta que se reciba otro mensaje
+    vTaskSuspend(NULL);
+  }  
 }
 
 void send_packet(void *pvParameters){
   while(1){
     //Byte array a enviar
     size_t size_data;
-    byte data[]={0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02,0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03}; 
+    byte data[]={0x01}; 
+    /*
+    100 bytes
+
+    0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02,0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05,
+    0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01, 0x05, 0x04, 0x03, 0x02, 0x01*/
 
     size_data = sizeof(data);
 
     //Llamo a la funcion que crea la trama de datos (payload)
     sendMessage(size_data, data);
-    Serial.println("Sending byte array");
+    Serial.println("Sending byte array de respuesta!");
+    Serial.println();
 
     //Espero 2 segundos luego de enviar mensaje
-    vTaskDelay(2000/portTICK_PERIOD_MS);
+    //vTaskDelay(2000/portTICK_PERIOD_MS);
 
-    //Suspendo esta tarea
-    //vTaskSuspend(NULL);
+    //vTaskResume(xHandle_poll_packet);
+
+    //Suspendo esta tarea hasta que se reciba otro mensaje
+    vTaskSuspend(NULL);
   }  
 }
-
-void onReceive(int packetSize) {
-  if (packetSize == 0) return;          // if there's no packet, return
-
-  // read packet header bytes:
-  int recipient = LoRa.read();          // recipient address
-  byte sender = LoRa.read();            // sender address
-  byte incomingMsgId = LoRa.read();     // incoming msg ID
-  byte incomingLength = LoRa.read();    // incoming msg length
-
-  String incoming = "";
-
-  while (LoRa.available()) {
-    incoming += (char)LoRa.read();
-  }
-
-  if (incomingLength != incoming.length()) {   // check length for error
-    Serial.println("error: message length does not match length");
-    return;                             // skip rest of function
-  }
-
-  // if the recipient isn't this device or broadcast,
-  if (recipient != localAddress && recipient != 0xFF) {
-    Serial.println("This message is not for me.");
-    return;                             // skip rest of function
-  }
-
-  // if message is for this device, or broadcast, print details:
-  Serial.println("Received from: 0x" + String(sender, HEX));
-  Serial.println("Sent to: 0x" + String(recipient, HEX));
-  Serial.println("Message ID: " + String(incomingMsgId));
-  Serial.println("Message length: " + String(incomingLength));
-  Serial.println("Message: " + incoming);
-  Serial.println("RSSI: " + String(LoRa.packetRssi()));
-  Serial.println("Snr: " + String(LoRa.packetSnr()));
-  Serial.println();
-}
-
 
 void setup() {
   Serial.begin(115200);                   // initialize serial
@@ -190,23 +221,13 @@ void setup() {
   Serial.println("LoRa init succeeded.");
 
   //Tareas de FreeRTOS corriendo en el nucleo 0 del ESP32
-
   xTaskCreatePinnedToCore(send_packet, "send_packet", 1024*2, NULL, 1, &xHandle_send_packet, 0);
-  //vTaskSuspend(xHandle_send_packet);
+  vTaskSuspend(xHandle_send_packet);
   xTaskCreatePinnedToCore(poll_packet, "poll_packet", 1024*2, NULL, 1, &xHandle_poll_packet, 0);
-
+  
   //xTaskCreatePinnedToCore(timer, "timer", 1024*2, NULL, 1, &xHandle_timer, 0);
 }
 
 void loop() {
-  // if (millis() - lastSendTime > interval) {
-  //   String message = "HeLoRa World!";   // send a message
-  //   sendMessage(message);
-  //   Serial.println("Sending " + message);
-  //   lastSendTime = millis();            // timestamp the message
-  //   interval = 2000;    // 2-3 seconds 
-  // }
-
-  // parse for a packet, and call onReceive with the result:
-  //onReceive(LoRa.parsePacket());
+  // Do nothing
 }
