4d3253323a3c764e0b104c63492442b2c0f7477d
alejandrotovar9 <jatovar02@gmail.com>
2024-04-23 23:22:16 -0400

    funcional envia float arrays a nodered

--- Pruebas-NodeRED-JSON-SendArrays/src/main.cpp
+++ Pruebas-NodeRED-JSON-SendArrays/src/main.cpp
@@ -7,11 +7,11 @@
 
 #define PI 3.14159265
 
-
 // WiFi and MQTT configuration
 // const char* ssid = "your_wifi_ssid";
 // const char* password = "your_wifi_password";
-const char* mqttBroker = "192.168.1.102";
+// const char* mqttBroker = "192.168.1.106";
+const char* mqttBroker = "192.168.113.64";
 //const char* mqttBroker = "192.168.20.64"; //TLF
 
 const int mqttPort = 1883;
@@ -25,71 +25,33 @@ const int   daylightOffset_sec = 3600;
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
-//Function to make an HTTP Get Request to get the JSON for the local time
-void getrequest(void){
+// Set your Static IP address
+IPAddress local_IP(192, 168, 1, 184);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 1, 1);
 
-    //Connect to HTTP server
-  WiFiClient client;
-  client.setTimeout(10000);
-  if (!client.connect("http://worldtimeapi.org", 80)) {
-    Serial.println(F("Connection failed"));
-    return;
+IPAddress subnet(255, 255, 0, 0);
+IPAddress primaryDNS(8, 8, 8, 8); // optional
+IPAddress secondaryDNS(8, 8, 4, 4); // optional
+
+
+//Creating a bigger array, with 500 elements
+const size_t ARRAY_SIZE = 1024;
+static float floatArrayX[ARRAY_SIZE];
+static float floatArrayY[ARRAY_SIZE];
+static float floatArrayZ[ARRAY_SIZE];
+
+int frequency1 = 50;
+int frequency2 = 100;
+int frequency3 = 200;
+
+//Function the fill the array with floats to form a sine wave
+void fillArray(float* array, size_t size){
+  for (size_t i = 0; i < size; i++) {
+    array[i] = sin(2 * PI * frequency1 * i / size) + 0.7*sin(2 * PI * frequency2 * i / size) + 0.5*sin(2 * PI * frequency3 * i / size);
   }
-
-  Serial.println(F("Connected!"));
-
-  // Send HTTP request
-  client.println(F("GET /api/timezone/America/Caracas HTTP/1.1"));
-  client.println(F("Host: worldtimeapi.org"));
-  client.println(F("Connection: close"));
-  if (client.println() == 0) {
-    Serial.println(F("Failed to send request"));
-    client.stop();
-    return;
-  }
-
-  // Check HTTP status
-  char status[32] = {0};
-  client.readBytesUntil('\r', status, sizeof(status));
-  // It should be "HTTP/1.0 200 OK" or "HTTP/1.1 200 OK"
-  if (strcmp(status + 9, "200 OK") != 0) {
-    Serial.print(F("Unexpected response: "));
-    Serial.println(status);
-    client.stop();
-    return;
-  }
-
-  // Skip HTTP headers
-  char endOfHeaders[] = "\r\n\r\n";
-  if (!client.find(endOfHeaders)) {
-    Serial.println(F("Invalid response"));
-    client.stop();
-    return;
-  }
-
-  // Allocate the JSON document
-  JsonDocument doc;
-
-  // Parse JSON object
-  DeserializationError error = deserializeJson(doc, client);
-  if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
-    client.stop();
-    return;
-  }
-
-  // Extract values
-  Serial.println(F("Response:"));
-  String datetime = doc["datetime"].as<String>();
-  Serial.println(datetime);
-
-  String date = datetime.substring(0, datetime.indexOf('T'));
-  Serial.println(date);
-
-  // Disconnect
-  client.stop();
 }
+
 
 void printLocalTime()
 {
@@ -109,19 +71,6 @@ void printLocalTime()
 
 }
 
-//Creating a bigger array, with 500 elements
-const size_t ARRAY_SIZE = 1024;
-static float floatArray[ARRAY_SIZE];
-int frequency1 = 50;
-int frequency2 = 100;
-int frequency3 = 200;
-
-//Function the fill the array with floats to form a sine wave
-void fillArray(float* array, size_t size){
-  for (size_t i = 0; i < size; i++) {
-    array[i] = sin(2 * PI * frequency1 * i / size) + 0.7*sin(2 * PI * frequency2 * i / size) + 0.5*sin(2 * PI * frequency3 * i / size);
-  }
-}
 
 // const size_t ARRAY_SIZE = 5;
 // float floatArray[ARRAY_SIZE] = {1.23, 4.56, 7.89, 10.11, 12.13};
@@ -129,6 +78,11 @@ void fillArray(float* array, size_t size){
 void setup_wifi() {
   delay(10);
   // We start by connecting to a WiFi network
+  // Configures static IP address
+  // if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+  //   Serial.println("STA Failed to configure");
+  // }
+
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -177,14 +131,9 @@ void setup() {
     }
   }
 
-  fillArray(floatArray, ARRAY_SIZE);
-
-  // //Print the array created
-  // for (size_t i = 0; i < ARRAY_SIZE; i++) {
-  //   Serial.print(floatArray[i]);
-  //   Serial.print(" ");
-  // }
-  // Serial.println();
+  fillArray(floatArrayX, ARRAY_SIZE);
+  fillArray(floatArrayY, ARRAY_SIZE);
+  fillArray(floatArrayZ, ARRAY_SIZE);
 
   mqttClient.setBufferSize(50000);
 
@@ -193,7 +142,6 @@ void setup() {
   Serial.println(mqttClient.getBufferSize());
 
 }
-
 
 void reconnect() {
   // Loop until we're reconnected
@@ -215,28 +163,49 @@ void reconnect() {
 
 void loop() {
   // Create JSON document to handle the 500 floats
-  DynamicJsonDocument doc(200);
-  //JsonDocument<200> doc;
-  JsonArray jsonArray = doc.to<JsonArray>();
+  // DynamicJsonDocument doc(200);
+  // //JsonDocument<200> doc;
+  // JsonArray jsonArray = doc.to<JsonArray>();
 
-  // Add float array values to JSON array
-  for (size_t i = 0; i < ARRAY_SIZE; i++) {
-    jsonArray.add(floatArray[i]);
-  }
+  // // Add float array values to JSON array
+  // for (size_t i = 0; i < ARRAY_SIZE; i++) {
+  //   jsonArray.add(floatArray[i]);
+  // }
 
-  // Serialize JSON to string
-  String jsonString;
-  serializeJson(jsonArray, jsonString);
+  // // Serialize JSON to string
+  // String jsonString;
+  // serializeJson(jsonArray, jsonString);
+
+  // Create a JSON document
+  // StaticJsonDocument<200> doc;
+
+  // Calculate the size of the JSON document
+  // This should be large enough to hold the entire JSON object
+  size_t jsonCapacity = JSON_ARRAY_SIZE(ARRAY_SIZE) * 3 + JSON_OBJECT_SIZE(3);
+
+  // Create a DynamicJsonDocument with the calculated capacity
+  DynamicJsonDocument doc(jsonCapacity);
+  
+// Add the float arrays to the document
+JsonArray arrayX = doc.createNestedArray("x");
+JsonArray arrayY = doc.createNestedArray("y");
+JsonArray arrayZ = doc.createNestedArray("z");
+
+for (size_t i = 0; i < ARRAY_SIZE; i++) {
+  arrayX.add(floatArrayX[i]);
+  arrayY.add(floatArrayY[i]);
+  arrayZ.add(floatArrayZ[i]);
+}
+  
+  // Serialize the document to a string
+  String json;
+  serializeJson(doc, json);
 
   //Print the size of the JSON Object in bytes
   Serial.println("Size of the JSON Object: ");
-  Serial.println(jsonString.length());
+  Serial.println(json.length());
 
-  //Print size of header
-  //Serial.println("Size of the Header of the Object: ");
-
-    // Publish JSON string to MQTT topic
-  if(mqttClient.publish(mqttTopic1, jsonString.c_str())){
+  if(mqttClient.publish(mqttTopic1, json.c_str())){
     Serial.println("Message published to MQTT topic");
   } else {
     Serial.println("Error publishing message to MQTT topic");
@@ -244,6 +213,19 @@ void loop() {
       reconnect();
     }
   }
+
+  //Print size of header
+  //Serial.println("Size of the Header of the Object: ");
+
+    // Publish JSON string to MQTT topic
+  // if(mqttClient.publish(mqttTopic1, jsonString.c_str())){
+  //   Serial.println("Message published to MQTT topic");
+  // } else {
+  //   Serial.println("Error publishing message to MQTT topic");
+  //   if((!wifiClient.connected())){
+  //     reconnect();
+  //   }
+  // }
 
   // Wait for some time before publishing again
   delay(5000);
